736408b21f83f6f1bad7d5af6818868aaa3ac1be
Mathieu <mathieu.herranzperez@gmail.com>
2018-12-12 21:11:08 -0600

    Map + enemy path

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [Range(1f, 50f)]
+    public float speed = 10f;
+
+
+    private Transform targetWaypoint;
+    private int wavepointIndex = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        targetWaypoint = Waypoints.pointArray[0];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 direction = targetWaypoint.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, targetWaypoint.position) <= 0.3f)
+        {
+            GetNextWaypoint();
+        }
+	}
+
+    private void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.pointArray.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            targetWaypoint = Waypoints.pointArray[++wavepointIndex];
+        }
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+    public static Transform[] pointArray;
+
+    void Awake()
+    {
+        pointArray = new Transform[transform.childCount];   // size : number of waypoints
+
+        // add all waypoints in the array
+        for(int i = 0; i < pointArray.Length; ++i)
+        {
+            pointArray[i] = transform.GetChild(i);
+        }
+    }
+}
