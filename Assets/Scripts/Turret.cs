9ce920fca308676b4242b3d541266306595baa4b
Mathieu <mathieu.herranzperez@gmail.com>
2018-12-12 22:45:16 -0600

    Turret rotation

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    public float range = 15f;
+    public string enemyTag = "Enemy";
+    public Transform partToRotate;
+    [Range(1f, 30f)]
+    public float turnSpeed = 10f;
+
+    private Transform target;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(target != null)
+        {
+            Vector3 direction = target.position - transform.position;   // direction to the target
+
+            // follow the target
+            Quaternion lookRatation = Quaternion.LookRotation(direction);
+            Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRatation, Time.deltaTime * turnSpeed).eulerAngles;
+            partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        }
+	}
+
+    private void UpdateTarget()
+    {
+        GameObject[] enemyArray = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        // find the nearest enemy
+        foreach (GameObject enemy in enemyArray)
+        {
+            // get ditance between the enemy and this
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    /**
+     *  Show the range in gizmos
+     */
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5.5f;
+    public Text waveCountdownText;
+
+    private float countdown = 2f;
+    private int waveNumber = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countdown).ToString();      // update the countdown UI text
+	}
+
+    IEnumerator SpawnWave()
+    {
+        ++waveNumber;
+        for (int i = 0; i < waveNumber; ++i)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);      // wait 0.5s to spawn another
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
