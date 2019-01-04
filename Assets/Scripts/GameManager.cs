a0abed24e53cfcbf077f760f0e33d7c177ef9aec
Mathieu <mathieu.herranzperez@gmail.com>
2019-01-04 09:40:53 -0600

    Flash when win

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
@@ -6,6 +8,10 @@ public class GameManager : MonoBehaviour {
 
     public GameObject gameOverUI;
     public GameObject winLevelUI;
+
+    public Image winBackgroundFlashImage;
+    public AnimationCurve curveOut;
+    public AnimationCurve curveIn;
 
     // Use this for initialization
     void Start ()
@@ -33,9 +39,43 @@ public class GameManager : MonoBehaviour {
 
     public void WinLevel()
     {
-        MouseManager.lockMouse = false;                 // unlock the cursor
-        GameIsOver = true; 
         winLevelUI.SetActive(true);
-        Time.timeScale = 0f;                            // freeze the game
+        StartCoroutine(FlashInput());
+    }
+
+
+    IEnumerator FlashInput()
+    {
+        // save the InputField.textComponent color
+        Color defaultColor = winBackgroundFlashImage.color;
+
+
+        float t = 0f;
+        while (t < 0.5f)
+        {
+            t += Time.deltaTime;
+            float alpha = curveOut.Evaluate(t);
+            winBackgroundFlashImage.color = new Color(1f, 1f, 1f, alpha);
+            yield return 0;             // skip to the next frame
+        }
+        t = 1f;
+        while (t > 0f)
+        {
+            t -= Time.deltaTime;
+            float alpha = curveIn.Evaluate(t);
+            winBackgroundFlashImage.color = new Color(1f, 1f, 1f, alpha);
+            yield return 0;             // skip to the next frame
+        }
+        
+        Destroy(winBackgroundFlashImage.gameObject); // magic door closes - remove object
+
+        WinLevelFct();
+    }
+
+    public void WinLevelFct()
+    {
+        MouseManager.lockMouse = false;                 // unlock the cursor
+        GameIsOver = true;
+        Time.timeScale = 0f;
     }
 }
